deeee6f5d3cea681da0fb14664dafb5f710c51c3
chrptos <chrptos@gmail.com>
2023-04-17 22:05:40 +0900

    firestore, createpost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,42 @@
 import React from 'react'
+import { useState } from 'react';
 import './CreatePost.css';
+import { addDoc, collection } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
 
 const CreatePost = () => {
+    const [title, setTitle] = useState();
+    const [postText, setPostText] = useState();
+    const navigate = useNavigate();
+
+    const createPost = async () => {
+        await addDoc(collection(db, "posts"), {
+            title: title,
+            postsText: postText,
+            author: {
+                username: auth.currentUser.displayName,
+                id: auth.currentUser.uid
+            }
+        })
+
+        navigate('/');
+    }
+
     return (
         <div className='create__post__page'>
             <div className='post__container'>
                 <h1>記事を投稿する</h1>
                 <div className="input__post">
                     <div>タイトル</div>
-                    <input type='text' placeholder='タイトルを記入' />
+                    <input type='text' placeholder='タイトルを記入' onChange={(e) => setTitle(e.target.value)} />
                 </div>
                 <div className="input__post">
                     <div>投稿</div>
-                    <textarea placeholder='投稿内容を記入' />
+                    <textarea placeholder='投稿内容を記入' onChange={(e) => setPostText(e.target.value)} />
                 </div>
-                <button className='post__button'>投稿する</button>
+                <button className='post__button' onClick={createPost}>投稿する</button>
             </div>
         </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Home.css';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Home = () => {
+    const [postList, setPostList] = useState();
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(collection(db, "posts"))
+            // console.log(data.docs.map((doc) => ({ doc })))
+            // ドキュメントに対してdata()を使うと簡単にデータが取得できる。
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        }
+        getPosts();
+        // console.log(postList)
+    }, []);
     return (
-        <div>Home</div>
+        <div className='homePage'>
+            {postList.map((post) => {
+                return (
+                    <div className="post__contents">
+                        <div className="post__header">
+                            <h1>{post.title}</h1>
+                        </div>
+                        <div className="post__text__container">
+                            {post.postsText}
+                        </div>
+                        <div className="name__and__delete__button">
+                            <h3>{post.author.username}</h3>
+                            <button>削除</button>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
